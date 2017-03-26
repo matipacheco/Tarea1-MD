cbfe3fa3acc24ec393ef16b083ae921f85b3401e
Mati <matipacheco@gmail.com>
2017-03-26 14:47:42 -0300

    kmeans funciona

--- kmeans.py
+++ kmeans.py
@@ -7,7 +7,7 @@ from sklearn.decomposition import PCA
 from drink_beer import *
 
 reviews 		= drink([])
-labels, reviews = set_reviews(reviews)
+labels, reviews = format_reviews(reviews)
 
 # print labels
 # print reviews[0]
@@ -20,14 +20,34 @@ k_means.fit(reviews)
 k_means_labels 			= k_means.labels_
 k_means_cluster_centers = k_means.cluster_centers_
 
-# fig = plt.figure(figsize=(3,3))
-# colors = ["#4EACC5","#FF9C34","#4E9A06"]
-# ax = fig.add_subplot(1,1,1)
-# ax.set_title("KMeans")
-# for k, col in zip(range(3), colors):
-# 	my_members = k_means_labels == k
-# 	ax.plot(X[my_members, 0], X[my_members, 1], "w", markerfacecolor=col, marker=".")
+# Step size of the mesh. Decrease to increase the quality of the VQ.
+h = 1000
 
-# ax.set_xticks(())
-# ax.set_yticks(())
-# plt.show()
+# Plot the decision boundary. For that, we will assign a color to each
+x_min, x_max = reviews[:, 0].min() - 1, reviews[:, 0].max() + 1
+y_min, y_max = reviews[:, 1].min() - 1, reviews[:, 1].max() + 1
+xx   , yy    = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+# Obtain labels for each point in mesh. Use last trained model.
+Z = k_means.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+
+plt.figure(1)
+plt.clf()
+plt.imshow(Z, interpolation = 'nearest', extent = (xx.min(), xx.max(), yy.min(), yy.max()),
+			  cmap = plt.cm.Paired, aspect = 'auto', origin = 'lower')
+
+plt.plot(reviews[:, 0], reviews[:, 1], 'k.', markersize = 2)
+# Plot the centroids as a white X
+centroids = k_means.cluster_centers_
+
+plt.scatter(centroids[:, 0], centroids[:, 1], marker = 'x', s = 169, linewidths = 3, color = 'w', zorder = 10)
+plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
+          'Centroids are marked with white cross')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
