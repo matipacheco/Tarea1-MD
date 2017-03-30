b84f4557ae0f25ed3aff5e74ff6dcfacc75169c8
Mati <matipacheco@gmail.com>
2017-03-30 20:40:43 -0300

    minibatch, opciones de ejecución al inicio y nuevo método en la librería

--- drink_beer.py
+++ drink_beer.py
@@ -12,7 +12,13 @@ def drink(reviews):
 def empty(string):
 	return not string
 
-def format_reviews(reviews):
+#########################################################################################
+# Retorna las columnas
+# brewery_id, review_time, review_overall, review_aroma, review_appearance, review_palate
+# review_taste, beer_abv, beer_beerid.
+#########################################################################################
+
+def get_only_review_numbers(reviews):
 	labels 	= reviews.pop(0)
 
 	for review in reviews:
@@ -70,3 +76,55 @@ def format_reviews(reviews):
 	labels.pop(1)
 
 	return labels, reviews
+
+#########################################################################################
+# Retorna las columnas
+# review_overall, review_aroma, review_appearance, review_palate, review_taste.
+#########################################################################################
+
+def get_only_review_scores(reviews):
+	labels 	= reviews.pop(0)
+
+	for review in reviews:
+		if empty(review[3]):
+			review[3]  = 0.0
+		else:
+			review[3]  = float(review[3])
+
+		if empty(review[4]):
+			review[4]  = 0.0
+		else:
+			review[4]  = float(review[4])
+
+		if empty(review[5]):
+			review[5]  = 0.0
+		else:
+			review[5]  = float(review[5])
+
+		if empty(review[7]):
+			review[7]  = 0.0
+		else:
+			review[7]  = float(review[7])
+
+		if empty(review[8]):
+			review[8]  = 0.0
+		else:
+			review[8]  = float(review[8])
+	
+		review.pop(11)
+		review.pop(10)
+		review.pop(9)
+		review.pop(6)
+		review.pop(2)
+		review.pop(1)
+		review.pop(0)
+
+	labels.pop(11)
+	labels.pop(10)
+	labels.pop(9)
+	labels.pop(6)
+	labels.pop(2)
+	labels.pop(1)
+	labels.pop(0)	
+
+	return labels, reviews	
--- minibatch.py
+++ minibatch.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 from sklearn.decomposition import PCA
 
 from drink_beer import *
@@ -20,12 +20,13 @@ else:
 	labels, reviews = get_only_review_scores(reviews)
 
 ## Para reducir la dimensionalidad y poder dibujar los clusters
-reviews = PCA(n_components = 2).fit_transform(reviews)
-k_means = KMeans(init = "k-means++", n_clusters = n_clusters, n_init = 100)
+reviews 	 = PCA(n_components = 2).fit_transform(reviews)
+mini_batch = MiniBatchKMeans(init = 'k-means++', n_clusters = n_clusters,
+														 batch_size = 10000, n_init = 100, max_no_improvement = 10)
 
-k_means.fit(reviews)
-k_means_labels 			= k_means.labels_
-k_means_cluster_centers = k_means.cluster_centers_
+mini_batch.fit(reviews)
+mini_batch_labels 					= mini_batch.labels_
+mini_batch_cluster_centers 	= mini_batch.cluster_centers_
 
 # Step size of the mesh. Decrease to increase the quality of the VQ.
 h = 1000
@@ -36,7 +37,7 @@ y_min, y_max = reviews[:, 1].min() - 1, reviews[:, 1].max() + 1
 xx   , yy    = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
 # Obtain labels for each point in mesh. Use last trained model.
-Z = k_means.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = mini_batch.predict(np.c_[xx.ravel(), yy.ravel()])
 
 # Put the result into a color plot
 Z = Z.reshape(xx.shape)
@@ -48,10 +49,10 @@ plt.imshow(Z, interpolation = 'nearest', extent = (xx.min(), xx.max(), yy.min(),
 
 plt.plot(reviews[:, 0], reviews[:, 1], 'k.', markersize = 2)
 # Plot the centroids as a white X
-centroids = k_means.cluster_centers_
+centroids = mini_batch.cluster_centers_
 
 plt.scatter(centroids[:, 0], centroids[:, 1], marker = 'x', s = 169, linewidths = 3, color = 'w', zorder = 10)
-plt.title("Clustering K-means con " + str(n_clusters) + " clusters (reducido utilizando PCA)\n"
+plt.title("Clustering MiniBatch K-means con " + str(n_clusters) + " clusters (reducido utilizando PCA)\n"
           "Los centroides estan marcados con una X")
 plt.xlim(x_min, x_max)
 plt.ylim(y_min, y_max)
