d4c00cb24ddfd99edc8314121510a6de8279bb69
Mati <matipacheco@gmail.com>
2017-04-01 21:23:31 -0300

    minibatch con panda

--- minibatch.py
+++ minibatch.py
@@ -1,27 +1,31 @@
 import numpy as np
+import pandas as pd
+
 import matplotlib.pyplot as plt
 
-from sklearn.cluster import MiniBatchKMeans
+from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
-from drink_beer import *
-
-print "[0] Para tomar solo los numeros del dataset"
-print "[1] Para tomar solo los scores del dataset"
+print "[0] Para clusterizar por la etiqueta Nombre Cerveceria"
+print "[1] Para clusterizar por la etiqueta Tipo de Cerveza"
 
 case 			 = eval(raw_input("\n Ingrese opcion: "))
 n_clusters = eval(raw_input("\n Ingrese la cantidad de clusters: "))
 
-reviews = drink([])
-labels  = drink_labels(reviews)
+dataset = pd.read_csv('beer_reviews.csv')
 
 if case == 0:
-	reviews = drink_review_numbers(reviews)
+	reviews = pd.DataFrame({'brewery_id':dataset['brewery_id'],'review_overall':dataset['review_overall'],
+                          'review_aroma':dataset['review_aroma'],'review_appearance':dataset['review_appearance'],
+                          'review_palate':dataset['review_palate'],'review_taste':dataset['review_taste'],'beer_abv':dataset['beer_abv']})
 else:
-	reviews = drink_review_scores(reviews)
+	reviews = pd.DataFrame({'beer_beerid':dataset['beer_beerid'],'review_overall':dataset['review_overall'],
+                          'review_aroma':dataset['review_aroma'],'review_appearance':dataset['review_appearance'],
+                          'review_palate':dataset['review_palate'],'review_taste':dataset['review_taste'],'beer_abv':dataset['beer_abv']})
 
-## Para reducir la dimensionalidad y poder dibujar los clusters
-reviews 	 = PCA(n_components = 2).fit_transform(reviews)
+reviews = reviews.fillna(0)
+
+reviews    = PCA(n_components = 2).fit_transform(reviews)
 mini_batch = MiniBatchKMeans(init = 'k-means++', n_clusters = n_clusters,
 														 batch_size = 10000, n_init = 100, max_no_improvement = 10)
 mini_batch.fit(reviews)
