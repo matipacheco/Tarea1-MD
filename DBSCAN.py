ce141d64f1dc2676865371e55d9c42742f1383ba
JuanPabloCastillo <jpcv12@hotmail.com>
2017-04-07 00:35:40 -0300

    Codigo con NMI

--- DBSCAN.py
+++ DBSCAN.py
@@ -4,51 +4,43 @@ import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import normalized_mutual_info_score as nmi
 import matplotlib.pyplot as plt
-
-print "[0] Para clusterizar por la etiqueta Nombre Cerveceria"
-print "[1] Para clusterizar por la etiqueta Tipo de Cerveza"
-
-case 			 = eval(raw_input("\n Ingrese opcion: "))
-
-print "[0] Para clusterizar sin normalizar"
-print "[1] Para clusterizar normalizando"
-
-case_n 			 = eval(raw_input("\n Ingrese opcion: "))
 
 min_samples = eval(raw_input("\n Ingrese la cantidad de min_samples: "))
 eps = eval(raw_input("\n Ingrese el valor de eps: "))
 start_time = time.time()
 dataset = pd.read_csv('beer_reviews.csv')
 
-if case == 0:
-	reviews = pd.DataFrame({'brewery_id':dataset['brewery_id'],'review_overall':dataset['review_overall'],
-                          'review_aroma':dataset['review_aroma'],'review_appearance':dataset['review_appearance'],
-                          'review_palate':dataset['review_palate'],'review_taste':dataset['review_taste'],'beer_abv':dataset['beer_abv']})
-else:
-	reviews = pd.DataFrame({'beer_beerid':dataset['beer_beerid'],'review_overall':dataset['review_overall'],
-                          'review_aroma':dataset['review_aroma'],'review_appearance':dataset['review_appearance'],
-                          'review_palate':dataset['review_palate'],'review_taste':dataset['review_taste'],'beer_abv':dataset['beer_abv']})
+breweries = pd.DataFrame({'brewery_id': dataset['brewery_id']})
+breweries = breweries.loc[0:249999, :]
+breweries = list(breweries.values.flatten())
+beers = pd.DataFrame({'beer_beerid': dataset['beer_beerid']})
+beers = beers.loc[0:249999, :]
+beers = list(beers.values.flatten())
 
+reviews = pd.DataFrame({'review_overall': dataset['review_overall'], 'review_aroma': dataset['review_aroma'],'review_appearance': dataset['review_appearance'],
+                        'review_palate': dataset['review_palate'], 'review_taste': dataset['review_taste'],'beer_abv': dataset['beer_abv']})
 reviews = reviews.fillna(0)
-reviews = reviews.loc[100000:249999,:]
+reviews = reviews.loc[0:249999, :]
 print "Comienzo de fitear DBSCAN"
 print("--- %s segundos ---" % (time.time() - start_time))
-
-if case_n == 1:
-    reviews = StandardScaler().fit_transform(reviews)
-
+reviews = StandardScaler().fit_transform(reviews)
 reviews    = PCA(n_components = 2).fit_transform(reviews)
 db = DBSCAN(eps=eps, min_samples=min_samples, algorithm="ball_tree").fit(reviews)
 print "Termine de fitear DBSCAN"
 print("--- %s segundos ---" % (time.time() - start_time))
-
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
 labels = db.labels_
 
+labels_n = list(db.labels_)
 # Number of clusters in labels, ignoring noise if present.
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+print "\nmin_samples: "+ str(min_samples) +" - eps: "+ str(eps) +" - numero de clusters: "+str(n_clusters_)
+print "Indice NMI para la etiqueta Nombre Cerveceria: " + str(nmi(breweries, labels_n))
+print "Indice NMI para la etiqueta Tipo Cerveza: " + str(nmi(beers, labels_n))
+print "------------------------------------------------------------\n"
 
 unique_labels = set(labels)
 colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
--- Ward.py
+++ Ward.py
@@ -4,45 +4,38 @@ import numpy as np
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import normalized_mutual_info_score as nmi
 import matplotlib.pyplot as plt
-
-print "[0] Para clusterizar por la etiqueta Nombre Cerveceria"
-print "[1] Para clusterizar por la etiqueta Tipo de Cerveza"
-
-case 			 = eval(raw_input("\n Ingrese opcion: "))
-
-print "[0] Para clusterizar sin normalizar"
-print "[1] Para clusterizar normalizando"
-
-case_n 			 = eval(raw_input("\n Ingrese opcion: "))
 
 n_clusters = eval(raw_input("\n Ingrese la cantidad de clusters: "))
 start_time = time.time()
 dataset = pd.read_csv('beer_reviews.csv')
 
-if case == 0:
-	reviews = pd.DataFrame({'brewery_id':dataset['brewery_id'],'review_overall':dataset['review_overall'],
-                          'review_aroma':dataset['review_aroma'],'review_appearance':dataset['review_appearance'],
-                          'review_palate':dataset['review_palate'],'review_taste':dataset['review_taste'],'beer_abv':dataset['beer_abv']})
-else:
-	reviews = pd.DataFrame({'beer_beerid':dataset['beer_beerid'],'review_overall':dataset['review_overall'],
-                          'review_aroma':dataset['review_aroma'],'review_appearance':dataset['review_appearance'],
-                          'review_palate':dataset['review_palate'],'review_taste':dataset['review_taste'],'beer_abv':dataset['beer_abv']})
+breweries = pd.DataFrame({'brewery_id': dataset['brewery_id']})
+breweries = breweries.loc[0:9999, :]
+breweries = list(breweries.values.flatten())
+beers = pd.DataFrame({'beer_beerid': dataset['beer_beerid']})
+beers = beers.loc[0:9999, :]
+beers = list(beers.values.flatten())
 
+reviews = pd.DataFrame({'review_overall': dataset['review_overall'], 'review_aroma': dataset['review_aroma'],'review_appearance': dataset['review_appearance'],
+                        'review_palate': dataset['review_palate'], 'review_taste': dataset['review_taste'],'beer_abv': dataset['beer_abv']})
 reviews = reviews.fillna(0)
-reviews = reviews.loc[0:8999,:]
+reviews = reviews.loc[0:9999,:]
 print "Comienzo de fitear ward"
 print("--- %s segundos ---" % (time.time() - start_time))
-
-if case_n == 1:
-    reviews = StandardScaler().fit_transform(reviews)
-
+reviews = StandardScaler().fit_transform(reviews)
 reviews    = PCA(n_components = 2).fit_transform(reviews)
 ward = AgglomerativeClustering(n_clusters = n_clusters, linkage='ward').fit(reviews)
 print "Termine de fitear ward"
 print("--- %s segundos ---" % (time.time() - start_time))
-
 label = ward.labels_
+labels_n = list(ward.labels_)
+print "\nnumero de clusters: "+str(n_clusters)
+print "Indice NMI para la etiqueta Nombre Cerveceria: " + str(nmi(breweries, labels_n))
+print "Indice NMI para la etiqueta Tipo Cerveza: " + str(nmi(beers, labels_n))
+print "------------------------------------------------------------\n"
+
 for l in np.unique(label):
     plt.plot(reviews[label == l, 0], reviews[label == l, 1],'o', color=plt.cm.jet(np.float(l) / np.max(label + 1)))
 plt.title('Ward con %d clusters' %n_clusters)
